f44569cdf3ca17b17bd28f37ba74ed0ef91f4fe0
ft972 <107643889+ft972@users.noreply.github.com>
2022-07-11 13:09:06 +0200

    Reduce support to Debian/Ubuntu

--- 1_system_packages_install/install.sh
+++ 1_system_packages_install/install.sh
@@ -14,31 +14,11 @@ fi
 
 BIT=$(getconf LONG_BIT)
 
-apt-get --help &>/dev/null
-if [ $? = 0 ]
-then
-PKGMANAGER=apt
-fi
-
-pacman --help  &>/dev/null
-if [ $? = 0 ]
-then
-PKGMANAGER=pacman
-fi
-
-dnf --help  &>/dev/null
-if [ $? = 0 ]
-then
-PKGMANAGER=dnf
-fi
 
 
 clear
 echo "Installation of the necessary packages to install and run"
 echo "the DVBViewer Media Server."
-echo
-echo "The scripts were created under Linux Mint. In other distributions,"
-echo "other packages may be required."
 echo 
 echo "The following tools are used:"
 echo "a 32 bit version of Wine"
@@ -46,7 +26,7 @@ echo "yad wget tar pidof pgrep unzip wrestool icotool sed 7z"
 echo "cabextract rtcwake convert(imagemagick)"
 echo ""
 
-if [ $BIT = 64 ] && [ $PKGMANAGER = "apt" ]
+if [ $BIT = 64 ]
 then
 echo "64bit apt package manager found."
 echo "Add multiarch support."
@@ -64,7 +44,7 @@ echo "Ready"
 exit
 fi
 
-if [ $BIT = 32 ] && [ $PKGMANAGER = "apt" ]
+if [ $BIT = 32 ]
 then
 echo "32bit apt package manager found."
 echo ""
@@ -80,44 +60,4 @@ echo "Ready"
 exit
 fi
 
-if [ $BIT = 64 ] && [ $PKGMANAGER = "pacman" ]
-then
-echo "64bit pacman package manager found."
-echo ""
-echo "Update package sources:"
-echo ""
-MULTILIB=$(sudo pacman -Syy|grep multilib)
-if [ -z "$MULTILIB" ]
-then
-    echo "Please enable multilib support and restart this script."
-    exit
-else
-echo ""
-echo "Install packages:"
-echo ""
-sudo pacman -S imagemagick wine wget unzip icoutils yad xorg-server-xvfb cabextract p7zip lib32-gstreamer
-echo ""
-echo "Ready"
-exit
-fi
-fi
-
-if [ $BIT = 64 ] && [ $PKGMANAGER = "dnf" ]
-then
-echo "64bit dnf package manager found."
-echo ""
-echo "Check for available updates:"
-echo ""
-sudo dnf check-update
-echo ""
-echo "Install packages:"
-echo ""
-sudo dnf install ImageMagick wine.i686 wget unzip icoutils yad xorg-x11-server-Xvfb cabextract p7zip p7zip-plugins gstreamer1.i686 gstreamer1-plugins-bad-free.i686 gstreamer1-plugins-bad-free-extras.i686 gstreamer1-plugins-base.i686 gstreamer1-plugins-good.i686 gstreamer1-plugins-good-extras.i686 gstreamer1-plugins-ugly-free.i686 mscore-fonts-all
-echo ""
-echo "Ready"
-exit
-fi
-
-echo "Package manager not recognized."
-echo "Please install dependencies manually via your package manager."
 
